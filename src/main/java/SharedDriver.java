856dc99b9de390292b5521a64abfa1a780def2bd
Kostiantyn1980 <Thecaty1980@gmail.com>
2023-11-18 12:26:19 -0700

    updated file shared driver

--- src/main/java/SharedDriver.java
+++ src/main/java/SharedDriver.java
@@ -36,6 +36,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -47,10 +48,21 @@ public class SharedDriver {
     public static WebDriver getWebDriver() {
         if (webDriver == null) {
             WebDriverManager.chromedriver().setup();
-            webDriver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--headless");
+
+            webDriver = new ChromeDriver(options);
             webDriver.manage().window().maximize();
             webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
+//            ChromeOptions options = new ChromeOptions();
+//                options.addArguments("--no-sandbox");
+//                options.addArguments("--disable-dev-shm-usage");
+//                options.addArguments("--headless");
+//
+//            WebDriverManager.chromedriver().setup();
+//            webDriver = new ChromeDriver(options);
         }
         return webDriver;
     }
